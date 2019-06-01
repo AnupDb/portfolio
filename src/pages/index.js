3f19bdffd8106660060cb25dac9b0f7ff0ed364a
Anup DebBarma <sunnyoranup@gmail.com>
2019-06-01 11:26:06 +0530

    Landing redesign

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,18 @@ from{
 }
 `
 
+const Floating = keyframes` 
+  from {
+    transform: translateY(0) rotate(0deg);
+    opacity: 0.5;
+    border-radius: 20%;
+  }
+  to {
+    transform: translateY(-1000px) rotate(720deg);
+    opacity: 0;
+    border-radius: 60%;
+  }
+`
 const IndexPage = ({ data }) => {
   return (
     <Layout>
@@ -24,12 +36,12 @@ const IndexPage = ({ data }) => {
         styles={css`
           .back {
             z-index: -1;
+            position:relative;
             height: 100vh;
-            background: url(${back}) no-repeat center center fixed;
-            -webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
+        background: rgb(142,197,252); /* Old browsers */
+        background: -moz-linear-gradient(45deg, rgba(142,197,252,1) 1%, rgba(224,195,252,1) 83%); /* FF3.6-15 */
+        background: -webkit-linear-gradient(45deg, rgba(142,197,252,1) 1%,rgba(224,195,252,1) 83%); /* Chrome10-25,Safari5.1-6 */
+        background: linear-gradient(45deg, rgba(142,197,252,1) 1%,rgba(224,195,252,1) 83%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
           }
           .front {
             z-index: 1;
@@ -82,10 +94,125 @@ const IndexPage = ({ data }) => {
             text-align: center;
             font-size: 1.4rem;
           }
+          .circles-container{
+            width:100%;
+            height:100vh;
+            position:absolute;
+            overflow:hidden;
+            .circle {
+                  position: absolute;
+                  display: block;
+                  list-style: none;
+                  width: 20px;
+                  height: 20px;
+                  background-color: rgba(255, 255, 255, 0.5);
+                  bottom: -100px;
+                  animation: ${Floating} 12s linear infinite;
+                  
+                  &:nth-child(1){
+                    left:10%;
+                    width:30px;
+                    height:30px;
+                    animation-delay:3000ms;
+                  }&:nth-child(2){
+                    left:30%;
+                    width:30px;
+                    height:30px;
+                    animation-delay:1000ms;
+                  }
+                 
+                 &:nth-child(3){
+                    left:60%;
+                    width:30px;
+                    height:30px;
+                    animation-delay:2000ms;
+                  }
+                   &:nth-child(4){
+                    left:90%;
+                    width:30px;
+                    height:30px;
+                    animation-delay:4000ms;
+                  }
+                  &:nth-child(5){
+                    left:50%;
+                    width:30px;
+                    height:30px;
+                    animation-delay:5000ms;
+                  }
+
+                  @media only screen and (min-width: 768px){
+                  
+                    &:nth-child(1){
+                    width:70px;
+                    height:70px;
+                    left:15%;
+                    animation-delay:1000ms;
+                  }
+                     &:nth-child(2){
+                    width:60px;
+                    height:60px;
+                    left:24%;
+                    animation-delay:3000ms;
+                  }
+                         &:nth-child(3){
+                    width:60px;
+                    height:60px;
+                    left:38%;
+                    animation-delay:6000ms;
+                  }      &:nth-child(4){
+                    width:60px;
+                    height:60px;
+                    left:55%;
+                    animation-delay:5000ms;
+                  }
+                  &:nth-child(5){
+                    width:80px;
+                    height:80px;
+                    left:70%;
+                    animation-delay:100ms;
+                  }&:nth-child(6){
+                    width:44px;
+                    height:44px;
+                    left:77%;
+                    animation-delay:3000ms;
+                  }&:nth-child(7){
+                    width:44px;
+                    height:44px;
+                    left:90%;
+                    animation-delay:2000ms;
+                  }&:nth-child(8){
+                    width:55px;
+                    height:55px;
+                    left:10%;
+                    animation-delay:4000ms;
+                  }
+                      &:nth-child(9){
+                    width:80px;
+                    height:80px;
+                    left:70%;
+                    animation-delay:100ms;
+                }
+                }
+            
+          }
         `}
       />
       <SEO title="AnupDev" keywords={[`gatsby`, `application`, `react`]} />
+
       <div className="back" id="home">
+        <div className="animation">
+          <ul className="circles-container">
+            <li className="circle" />
+            <li className="circle" />
+            <li className="circle" />
+            <li className="circle" />
+            <li className="circle" />
+            <li className="circle" />
+            <li className="circle" />
+            <li className="circle" />
+            <li className="circle" />
+          </ul>
+        </div>
         <div className="front">
           <p className="cta">
             Hello I'm Anup DebBarma
